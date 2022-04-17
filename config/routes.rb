bb8e23a044d17e9043b5e4484a2c5693b2bc9b02
Maria Khelli <>
2022-04-17 04:43:19 +0000

    generate controller food

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'food/index'
+  get 'food/show'
+  get 'food/new'
+  get 'food/edit'
+  get 'food/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
